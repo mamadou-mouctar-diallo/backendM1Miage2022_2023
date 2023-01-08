126a309a0a0e74f4f02fed4f96e8804c5f56fc44
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2023-01-08 13:34:40 +0100

    update set auth service , delete and update

--- api/assignment/routes/routesAssignment.js
+++ api/assignment/routes/routesAssignment.js
@@ -6,7 +6,7 @@ router.get('/assignments', (req, res) => getAssignments(req, res));
 
 router.post('/assignments/add',protectedRequest, (req, res) => postAssignment(req, res));
 
-router.delete('/asignments/delete/:id',protectedRequest, (req, res) => deleteAssignment(req, res));
+router.delete('/assignments/delete/:id',protectedRequest, (req, res) => deleteAssignment(req, res));
 
 router.put('/assignments/update/:id',protectedRequest, (req, res) => updateAssignment(req, res));
 
